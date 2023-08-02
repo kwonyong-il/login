500d7dfcba6fd0dc0f1999c8ca50397f45c74188
kwonyongil <kkwonyi95@gmail.com>
2023-08-02 17:11:38 +0900

    lv.1 finish

--- app/src/main/java/com/example/login/HomeActivity.kt
+++ app/src/main/java/com/example/login/HomeActivity.kt
@@ -2,10 +2,21 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val id = intent.getStringExtra("dataFromId")
+        val id_text = findViewById<EditText>(R.id.id_text)
+        id_text.setText(id)
+
+        val btn_close = findViewById<Button>(R.id.btn_close)
+        btn_close.setOnClickListener {
+            finish()
+        }
     }
 }
