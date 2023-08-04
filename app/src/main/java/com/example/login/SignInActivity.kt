abc6f25163d12f0aaf777f589b753991689e8638
kwonyongil <kkwonyi95@gmail.com>
2023-08-04 16:05:03 +0900

    lv.4 finish

--- app/src/main/java/com/example/login/SignInActivity.kt
+++ app/src/main/java/com/example/login/SignInActivity.kt
@@ -1,22 +1,26 @@
 package com.example.login
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var resultText: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         val idData = findViewById<EditText>(R.id.id_data)
         val passwordData = findViewById<EditText>(R.id.pw_data)
-        val btn1 = findViewById<Button>(R.id.btn_login)
+        val btnOne = findViewById<Button>(R.id.btn_login)
 
-        btn1.setOnClickListener {
+        btnOne.setOnClickListener {
             val id = idData.text.toString()
             val password = passwordData.text.toString()
             val intent = Intent(this, HomeActivity::class.java)
@@ -28,10 +32,22 @@ class SignInActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-        val btn2 = findViewById<Button>(R.id.btn_signup)
-        btn2.setOnClickListener {
+        resultText = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                val signedUpId = data?.getStringExtra("signedUpId")
+                val signedUpPassword = data?.getStringExtra("signedUpPassword")
+
+                // Use the data received from SignUpActivity (if needed)
+                idData.setText(signedUpId)
+                passwordData.setText(signedUpPassword)
+            }
+        }
+
+        val btnTwo = findViewById<Button>(R.id.btn_signup)
+        btnTwo.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            resultText.launch(intent)
         }
     }
 }
--- app/src/main/java/com/example/login/SignUpActivity.kt
+++ app/src/main/java/com/example/login/SignUpActivity.kt
@@ -1,11 +1,13 @@
 package com.example.login
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +27,10 @@ class SignUpActivity : AppCompatActivity() {
             if (signUpName.isEmpty() || signUpId.isEmpty() || signUpPassword.isEmpty()) {
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
             } else {
+                val intent = Intent()
+                intent.putExtra("signedUpId", signUpId)
+                intent.putExtra("signedUpPassword", signUpPassword)
+                setResult(Activity.RESULT_OK, intent)
                 Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                 finish()
             }
