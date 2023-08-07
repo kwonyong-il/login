157ad4ab5ca095c503456e1c33d5c42d483fc4d4
kwonyongil <kkwonyi95@gmail.com>
2023-08-07 14:55:34 +0900

    lv.5 finish + TextView fixed

--- app/src/main/java/com/example/login/HomeActivity.kt
+++ app/src/main/java/com/example/login/HomeActivity.kt
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val randomImage = listOf(
+            R.drawable.random1,
+            R.drawable.random2,
+            R.drawable.random3,
+            R.drawable.random4,
+            R.drawable.random5
+        )
+        val randomIndex = Random.nextInt(randomImage.size)
+        val imageRandom = findViewById<ImageView>(R.id.random_img)
+        imageRandom.setImageResource(randomImage[randomIndex])
+
+
         val id = intent.getStringExtra("dataFromId")
-        val idText = findViewById<EditText>(R.id.id_text)
-        idText.setText(id)
+        val idText = findViewById<TextView>(R.id.id_text)
+        idText.text = "아이디 : $id"
 
         val btnClose = findViewById<Button>(R.id.btn_close)
         btnClose.setOnClickListener {
